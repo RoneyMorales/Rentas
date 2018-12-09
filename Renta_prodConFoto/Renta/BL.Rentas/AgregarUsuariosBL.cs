a640477fb72c356d13c2954956c00e3e70f8e4f5
Roney Lobo <aresylobo@unah.hn>
2018-12-09 09:49:36 -0600

    Se agrego la opcion de fotos a los productos.

---
+++ Renta_prodConFoto/Renta/BL.Rentas/AgregarUsuariosBL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Rentas
+{
+    class AgregarUsuariosBL
+    {
+        contexto _contexto;
+        public BindingList<AgregarUsuariosBL> ListaUsuarios { get; set; }
+        public string Nombre { get; set; }
+        public string Contrasena { get; set; }
+    }
+}
